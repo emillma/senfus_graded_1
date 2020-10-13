d702f6a5e90af3622385a0df53cc7d4491c1771a
aHaugl <44579281+aHaugl@users.noreply.github.com>
2020-10-13 18:05:11 +0200

    Made plotting uniform with run_joyride

--- run_imm_pda.py
+++ run_imm_pda.py
@@ -17,58 +17,26 @@ import ekf
 import imm
 import pda
 import estimationstatistics as estats
+
 from plotting_utils import apply_settings, plot_cov_ellipse2d
+
+from plotting import (plot_measurements, plot_traj, plot_NEES_CI, plot_errors,
+                      plot_NIS_CV)
 
 # %% plot config check and style setup
 
 apply_settings()
 
 
-# %% load data and plot
+# %% load data
 filename_to_load = "data_for_imm_pda.mat"
 loaded_data = scipy.io.loadmat(filename_to_load)
 K = loaded_data["K"].item()
-Ts = loaded_data["Ts"].item()
+# Ts = loaded_data["Ts"].item()
+Ts = [loaded_data["Ts"].item() for i in range(K)]
 Xgt = loaded_data["Xgt"].T
 Z = [zk.T for zk in loaded_data["Z"].ravel()]
 true_association = loaded_data["a"].ravel()
-
-# plot measurements close to the trajectory
-fig1, ax1 = plt.subplots(num=1, clear=True)
-
-Z_plot_data = np.empty((0, 2), dtype=float)
-plot_measurement_distance = np.inf
-for Zk, xgtk in zip(Z, Xgt):
-    to_plot = np.linalg.norm(
-        Zk - xgtk[None:2], axis=1) <= plot_measurement_distance
-    Z_plot_data = np.append(Z_plot_data, Zk[to_plot], axis=0)
-
-ax1.scatter(*Z_plot_data.T, s=5, color="C1")
-ax1.plot(*Xgt.T[:2], color="C0", linewidth=1.5)
-ax1.set_title("True trajectory and the nearby measurements")
-plt.show(block=False)
-
-# %% play measurement movie. Remember that you can cross out the window
-play_movie = False
-play_slice = slice(0, K)
-if play_movie:
-    if "inline" in matplotlib.get_backend():
-        print("the movie might not play with inline plots")
-    fig2, ax2 = plt.subplots(num=2, clear=True)
-    sh = ax2.scatter(np.nan, np.nan, s=5)
-    th = ax2.set_title(f"measurements at step 0")
-    mins = np.vstack(Z).min(axis=0)
-    maxes = np.vstack(Z).max(axis=0)
-    ax2.axis([mins[0], maxes[0], mins[1], maxes[1]])
-    plotpause = 0.1
-    # sets a pause in between time steps if it goes to fast
-    for k, Zk in enumerate(Z[play_slice]):
-        sh.set_offsets(Zk)
-        th.set_text(f"measurements at step {k}")
-        fig2.canvas.draw_idle()
-        plt.show(block=False)
-        plt.pause(plotpause)
-
 
 # %% IMM-PDA
 
@@ -136,7 +104,7 @@ tracker_predict_list = []
 tracker_estimate_list = []
 # estimate
 for k, (Zk, x_true_k) in enumerate(zip(Z, Xgt)):
-    tracker_predict = tracker.predict(tracker_update, Ts)
+    tracker_predict = tracker.predict(tracker_update, Ts[k])
     tracker_update = tracker.update(Zk, tracker_predict)
 
     # You can look at the prediction estimate as well
@@ -188,154 +156,22 @@ ANEESpos = np.mean(NEESpos)
 ANEESvel = np.mean(NEESvel)
 ANEES = np.mean(NEES)
 
-# %% plots
+#Plots
 # trajectory
-fig3, axs3 = plt.subplots(1, 2, num=3, clear=True)
-axs3[0].plot(*x_hat.T[:2], label=r"$\hat x$")
-axs3[0].plot(*Xgt.T[:2], label="$x$")
-axs3[0].set_title(
-    f"RMSE(pos, vel) = ({posRMSE:.3f}, {velRMSE:.3f})\npeak_dev(pos, vel) = ({peak_pos_deviation:.3f}, {peak_vel_deviation:.3f})"
-)
-axs3[0].axis("equal")
-# probabilities
-axs3[1].plot(np.arange(K) * Ts, prob_hat)
-axs3[1].set_ylim([0, 1])
-axs3[1].set_ylabel("mode probability")
-axs3[1].set_xlabel("time")
+# #Fig 1
+plot_measurements(K, Ts, Xgt, Z)
 
-# NEES
-fig4, axs4 = plt.subplots(3, sharex=True, num=4, clear=True)
-axs4[0].plot(np.arange(K) * Ts, NEESpos)
-axs4[0].plot([0, (K - 1) * Ts], np.repeat(CI2[None], 2, 0), "--r")
-axs4[0].set_ylabel("NEES pos")
-inCIpos = np.mean((CI2[0] <= NEESpos) * (NEESpos <= CI2[1]))
-axs4[0].set_title(f"{inCIpos*100:.1f}% inside {confprob*100:.1f}% CI")
+# #Fig 3
+plot_traj(Ts, Xgt, x_hat, Z, posRMSE, velRMSE, prob_hat,
+              peak_pos_deviation, peak_vel_deviation
+              )
+# #Fig 4
+plot_NEES_CI(Ts, NEESpos, ANEESpos, NEESvel, ANEESvel, NEES, ANEES,
+              CI2, CI4, CI2K, CI4K, confprob)
 
-axs4[1].plot(np.arange(K) * Ts, NEESvel)
-axs4[1].plot([0, (K - 1) * Ts], np.repeat(CI2[None], 2, 0), "--r")
-axs4[1].set_ylabel("NEES vel")
-inCIvel = np.mean((CI2[0] <= NEESvel) * (NEESvel <= CI2[1]))
-axs4[1].set_title(f"{inCIvel*100:.1f}% inside {confprob*100:.1f}% CI")
-
-axs4[2].plot(np.arange(K) * Ts, NEES)
-axs4[2].plot([0, (K - 1) * Ts], np.repeat(CI4[None], 2, 0), "--r")
-axs4[2].set_ylabel("NEES")
-inCI = np.mean((CI2[0] <= NEES) * (NEES <= CI2[1]))
-axs4[2].set_title(f"{inCI*100:.1f}% inside {confprob*100:.1f}% CI")
-
-print(f"ANEESpos = {ANEESpos:.2f} with CI = [{CI2K[0]:.2f}, {CI2K[1]:.2f}]")
-print(f"ANEESvel = {ANEESvel:.2f} with CI = [{CI2K[0]:.2f}, {CI2K[1]:.2f}]")
-print(f"ANEES = {ANEES:.2f} with CI = [{CI4K[0]:.2f}, {CI4K[1]:.2f}]")
-
-# errors
-fig5, axs5 = plt.subplots(2, num=5, clear=True)
-axs5[0].plot(np.arange(K) * Ts,
-             np.linalg.norm(x_hat[:, :2] - Xgt[:, :2], axis=1))
-axs5[0].set_ylabel("position error")
-
-axs5[1].plot(np.arange(K) * Ts,
-             np.linalg.norm(x_hat[:, 2:4] - Xgt[:, 2:4], axis=1))
-axs5[1].set_ylabel("velocity error")
+#Fig 5 errors
+plot_errors(Ts, Xgt, x_hat, CI2, CI4, CI2K, CI4K, confprob)
 
 
-# %% TBD: estimation "movie"
-
-
-play_estimation_movie = False
-mTL = 0.2  # maximum transparancy (between 0 and 1);
-plot_pause = 1  # lenght to pause between time steps;
-start_k = 1
-end_k = K
-plot_range = slice(start_k, end_k)  # the range to go through
-
-# %k = 31; assert(all([k > 1, k <= K]), 'K must be in proper range')
-fig6, axs6 = plt.subplots(1, 2, num=6, clear=True)
-mode_lines = [axs6[0].plot(np.nan, np.nan, color=f"C{s}")[0] for s in range(2)]
-meas_sc = axs6[0].scatter(np.nan, np.nan, color="r", marker="x")
-meas_sc_true = axs6[0].scatter(np.nan, np.nan, color="g", marker="x")
-min_ax = np.vstack(Z).min(axis=0)  # min(cell2mat(Z'));
-max_ax = np.vstack(Z).max(axis=0)  # max(cell2mat(Z'));
-axs6[0].axis([min_ax[0], max_ax[0], min_ax[1], max_ax[0]])
-
-for k, (Zk, pred_k, upd_k, ak) in enumerate(
-    zip(
-        Z[plot_range],
-        tracker_predict_list[plot_range],
-        tracker_update_list[plot_range],
-        true_association[plot_range],
-    ),
-    start_k,
-):
-    # k, (Zk, pred_k, upd_k, ak) = data
-    (ax.cla() for ax in axs6)
-    pl = []
-    # probbar(:, k), xbar(:, :, k), Pbar(:, :, :, k));
-    gated = tracker.gate(Zk, pred_k)
-    minG = 1e20 * np.ones(2)
-    maxG = np.zeros(2)
-    cond_upd_k = tracker.conditional_update(Zk[gated], pred_k)
-    beta_k = tracker.association_probabilities(Zk[gated], pred_k)
-    for s in range(2):
-        mode_lines[s].set_data = (
-            np.array([u.components[s].mean[:2]
-                      for u in tracker_update_list[:k]]).T,
-        )
-        axs6[1].plot(prob_hat[: (k - 1), s], color=f"C{s}")
-        for j, cuj in enumerate(cond_upd_k):
-            alpha = 0.7 * beta_k[j] * cuj.weights[s] + 0.3
-            # csj = mTL * co(s, :) + (1 - mTL) * (beta(j)*skupd(s, j)*co(s, :) + (1 - beta(j)*skupd(s, j)) * ones(1, 3)); % transparancy
-            upd_km1_s = tracker_update_list[k - 1].components[s]
-            pl.append(
-                axs6[0].plot(
-                    [upd_km1_s.mean[0], cuj.components[s].mean[0]],
-                    [upd_km1_s.mean[1], cuj.components[s].mean[1]],
-                    "--",
-                    color=f"C{s}",
-                    alpha=alpha,
-                )
-            )
-
-            pl.append(
-                axs6[1].plot(
-                    [k - 1, k],
-                    [prob_hat[k - 1, s], cuj.weights[s]],
-                    color=f"C{s}",
-                    alpha=alpha,
-                )
-            )
-            # axis([minAx(1), maxAx(1), minAx(2), maxAx(2)])
-            # %alpha(pl, beta(j)*skupd(s, j));
-            # drawnow;
-            pl.append(
-                plot_cov_ellipse2d(
-                    axs6[0],
-                    cuj.components[s].mean[:2],
-                    cuj.components[s].cov[:2, :2],
-                    edgecolor=f"C{s}",
-                    alpha=alpha,
-                )
-            )
-
-        Sk = imm_filter.filters[s].innovation_cov([0, 0], pred_k.components[s])
-        # gateData = chol(Sk)' * [cos(thetas); sin(thetas)] * sqrt(tracker.gateSize) + squeeze(xbar(1:2, s, k));
-        # plot(gateData(1, :),gateData(2, :), '.--', 'Color', co(s,:))
-        pl.append(
-            plot_cov_ellipse2d(
-                axs6[0],
-                pred_k.components[s].mean[:2],
-                Sk,
-                n_sigma=tracker.gate_size,
-                edgecolor=f"C{s}",
-            )
-        )
-        meas_sc.set_offsets(Zk)
-        pl.append(axs6[0].scatter(*Zk.T, color="r", marker="x"))
-        if ak > 0:
-            meas_sc_true.set_offsets(Zk[ak - 1])
-        else:
-            meas_sc_true.set_offsets(np.array([np.nan, np.nan]))
-
-    plt.gcf().canvas.draw_idle()
-    plt.gcf().canvas.start_event_loop(0.01)
 plt.show()
 # %%
